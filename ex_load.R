4a672d7790225040c3877597203c2f3805c3c7ee
ewen_ <ewenhenderson@gmail.com>
2018-04-02 18:49:54 +0100

    add example collection data

---
+++ ex_load.R
@@ -0,0 +1,21 @@
+
+# SETUP -------------------------------------------------------------------
+
+# set traktor folder
+traktor_dir <- "/Users/Ewen/Documents/Native Instruments/Traktor 2.11.0/"
+
+# get functions
+source("extract_funcs.R")
+
+
+# LOAD --------------------------------------------------------------------
+
+# load traktor collection
+traktor_collection <- read_traktor_collection(x = "./example_data/traktor_collection.nml")
+
+# load rekordbox collection
+rekordbox_collection <- read_rekordbox_collection(x = "./example_data/rekordbox_collection.xml")
+
+
+
+
--- extract_funcs.R
+++ extract_funcs.R
@@ -1,3 +1,4 @@
+# function to read traktor (.nml) collection
 read_traktor_collection <- function(x) {
   
   # read collection file
@@ -29,6 +30,7 @@ read_traktor_collection <- function(x) {
   info_tree <- xml2::xml_find_first(collection_entries, xpath = ".//INFO")
   info <- tibble::tibble(
     genre = xml2::xml_attr(info_tree, "GENRE"),
+    release_year = xml2::xml_attr(info_tree, "RELEASE_DATE"),
     track_length = xml2::xml_attr(info_tree, "PLAYTIME"),
     play_count = xml2::xml_attr(info_tree, "PLAYCOUNT"),
     import_date = xml2::xml_attr(info_tree, "IMPORT_DATE"),
@@ -42,7 +44,42 @@ read_traktor_collection <- function(x) {
   # fix col classes
   data <- data %>%
     mutate_at(c("import_date", "last_played"), lubridate::ymd) %>%
-    mutate_at(c("bpm", "track_length", "play_count"), as.numeric)
+    mutate_at(c("bpm", "track_length", "play_count"), as.numeric) %>%
+    mutate(release_year = lubridate::year(lubridate::ymd(release_year)))
+  
+  return(data)
+  
+}
+
+
+# function to read rekordbox (.xml) collection
+read_rekordbox_collection <- function(x) {
+  
+  # read collection file
+  collection <- xml2::read_xml(x = x)
+  
+  # extract collection entries
+  collection_entries <- xml2::xml_child(collection, search = 2) %>%
+    xml2::xml_find_all(xpath = ".//TRACK")
+  
+  # extract attrs
+  data <- tibble::tibble(
+    track_title = xml2::xml_attr(collection_entries, "Name"),
+    artist_name = xml2::xml_attr(collection_entries, "Artist"),
+    album_title = xml2::xml_attr(collection_entries, "Album"),
+    bpm = xml2::xml_attr(collection_entries, "AverageBpm"),
+    genre = xml2::xml_attr(collection_entries, "Genre"),
+    release_year = xml2::xml_attr(collection_entries, "Year"),
+    track_length = xml2::xml_attr(collection_entries, "TotalTime"),
+    play_count = xml2::xml_attr(collection_entries, "PlayCount"),
+    import_date = xml2::xml_attr(collection_entries, "DateAdded"),
+    key = xml2::xml_attr(collection_entries, "Tonality")
+  )
+  
+  # fix col classes
+  data <- data %>%
+    mutate_at(c("bpm", "track_length", "play_count", "release_year"), as.numeric) %>%
+    mutate(import_date = lubridate::ymd(import_date))
   
   return(data)
   
