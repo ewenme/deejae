a6f41ac6eebb08668483e54d678bd4aedc1634f6
ewen_ <ewenhenderson@gmail.com>
2018-04-02 18:13:05 +0100

    draft func for extracting a users traktor collection

--- ex_workflow.R
+++ ex_workflow.R
@@ -1,18 +1,23 @@
 
 # SETUP -------------------------------------------------------------------
 
-library(xml2)
-library(dplyr)
+# set traktor folder
+traktor_dir <- "/Users/Ewen/Documents/Native Instruments/Traktor 2.11.0/"
 
-hist_dir <- "/Users/Ewen/Documents/Native Instruments/Traktor 2.11.0/History"
+# get functions
+source("extract_funcs.R")
+
 
 # LOAD --------------------------------------------------------------------
 
-# list files in traktor history dir
-hist_files <- list.files(hist_dir)
+# load collection
+traktor_collection <- read_traktor_collection(x = paste0(traktor_dir, "collection.nml"))
 
-# try reading one
-test <- read_xml(x=paste0(hist_dir, "/", hist_files[1]))
+# list files in traktor history dir
+traktor_hist_files <- list.files(paste0(traktor_dir, "History"))
+
+# try reading NML files
+nml_files <- map(hist_dir_files, function(x) read_xml(paste0(hist_dir, "/", x)))
 
 
 # CLEAN -------------------------------------------------------------------
--- extract_funcs.R
+++ extract_funcs.R
@@ -1,28 +1,49 @@
-
-extract_collection_entries <- function(x) {
+read_traktor_collection <- function(x) {
+  
+  # read collection file
+  collection <- xml2::read_xml(x = x)
   
   # extract collection entries
-  collection_entries <- xml2::xml_child(x, search = 3) %>%
+  collection_entries <- xml2::xml_child(collection, search = 3) %>%
     xml2::xml_find_all(xpath = ".//ENTRY")
   
   # extract parent attrs
-  parents <- data.frame(t(data.frame(as.list(xml2::xml_attrs(collection_entries)))), 
-                        stringsAsFactors = F, row.names = NULL)
-  
-  # extract child attrs
-  child_cats <- c("TEMPO", "MUSICAL_KEY")
-  
-  children <- lapply(child_cats, function(x) 
-    data.frame(t(data.frame(as.list(xml2::xml_attrs(xml2::xml_find_all(collection_entries,
-                                                                       xpath = paste0(".//", 
-                                                                                      x)))),
-                            stringsAsFactors = FALSE)), row.names = NULL)
+  parents <- tibble::tibble(
+    track_title = xml2::xml_attr(collection_entries, "TITLE"),
+    artist_name = xml2::xml_attr(collection_entries, "ARTIST")
   )
   
-  # bind child attrs
-  children <- bind_cols(children)
+  # extract album attrs
+  album_tree <- xml2::xml_find_first(collection_entries, xpath = ".//ALBUM")
+  album <- tibble::tibble(
+    album_title = xml2::xml_attr(album_tree, "TITLE")
+  )
   
-  # bind parent/child cols
-  entries <- bind_cols(parents, children)
+  # extract tempo attrs
+  tempo_tree <- xml2::xml_find_first(collection_entries, xpath = ".//TEMPO")
+  tempo <- tibble::tibble(
+    bpm = xml2::xml_attr(tempo_tree, "BPM")
+  )
+  
+  # extract info attrs
+  info_tree <- xml2::xml_find_first(collection_entries, xpath = ".//INFO")
+  info <- tibble::tibble(
+    genre = xml2::xml_attr(info_tree, "GENRE"),
+    track_length = xml2::xml_attr(info_tree, "PLAYTIME"),
+    play_count = xml2::xml_attr(info_tree, "PLAYCOUNT"),
+    import_date = xml2::xml_attr(info_tree, "IMPORT_DATE"),
+    last_played = xml2::xml_attr(info_tree, "LAST_PLAYED"),
+    key = xml2::xml_attr(info_tree, "KEY")
+  )
+  
+  # bind extracted features
+  data <- dplyr::bind_cols(parents, album, tempo, info)
+  
+  # fix col classes
+  data <- data %>%
+    mutate_at(c("import_date", "last_played"), lubridate::ymd) %>%
+    mutate_at(c("bpm", "track_length", "play_count"), as.numeric)
+  
+  return(data)
   
 }
