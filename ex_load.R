815ea295be0691ddcfdfdac46532c1c0dcd5741d
ewen_ <ewenhenderson@gmail.com>
2018-04-09 20:38:49 +0100

    make traktor history func

--- ex_load.R
+++ ex_load.R
@@ -1,8 +1,18 @@
 
 # SETUP -------------------------------------------------------------------
 
+# packages
+library(dplyr)
+library(xml2)
+library(lubridate)
+library(purrr)
+
 # set traktor folder
 traktor_dir <- "/Users/Ewen/Documents/Native Instruments/Traktor 2.11.0/"
+
+# get traktor history files
+traktor_history_files <- list.files(paste0(traktor_dir, "History/"),
+                                    full.names = TRUE)
 
 # get functions
 source("extract_funcs.R")
@@ -16,6 +26,6 @@ traktor_collection <- read_traktor_collection(x = "./example_data/traktor_collec
 # load rekordbox collection
 rekordbox_collection <- read_rekordbox_collection(x = "./example_data/rekordbox_collection.xml")
 
-
-
+# load traktor history
+playlist_data <- map_dfr(traktor_history_files, read_traktor_history)
 
--- extract_funcs.R
+++ extract_funcs.R
@@ -10,8 +10,18 @@ read_traktor_collection <- function(x) {
   
   # extract parent attrs
   parents <- tibble::tibble(
+    audio_id = xml2::xml_attr(collection_entries, "AUDIO_ID"),
     track_title = xml2::xml_attr(collection_entries, "TITLE"),
     artist_name = xml2::xml_attr(collection_entries, "ARTIST")
+  )
+  
+  # extract loaction attrs
+  location_tree <- xml2::xml_find_first(collection_entries, xpath = ".//LOCATION")
+  location <- tibble::tibble(
+    volume = xml2::xml_attr(location_tree, "VOLUME"),
+    dir = xml2::xml_attr(location_tree, "DIR"),
+    file = xml2::xml_attr(location_tree, "FILE"),
+    uid = paste0(volume, dir, file)
   )
   
   # extract album attrs
@@ -39,13 +49,62 @@ read_traktor_collection <- function(x) {
   )
   
   # bind extracted features
-  data <- dplyr::bind_cols(parents, album, tempo, info)
+  data <- dplyr::bind_cols(parents, location, album, tempo, info)
   
   # fix col classes
   data <- data %>%
     mutate_at(c("import_date", "last_played"), lubridate::ymd) %>%
     mutate_at(c("bpm", "track_length", "play_count"), as.numeric) %>%
     mutate(release_year = lubridate::year(lubridate::ymd(release_year)))
+  
+  return(data)
+  
+}
+
+
+# function to read traktor history (.nml) file
+read_traktor_history <- function(x) {
+  
+  # get track info ------------------------------------
+  
+  track_data <- read_traktor_collection(x)
+  
+  # get playlist info --------------------------------------
+  
+  playlist_data <- xml2::read_xml(x)
+  
+  playlist_entries <- xml2::xml_child(playlist_data, 5) %>%
+    xml2::xml_find_all(xpath = ".//ENTRY")
+  
+  # extract key attrs
+  key_tree <- xml2::xml_find_first(playlist_entries, xpath = ".//PRIMARYKEY")
+  key <- tibble::tibble(
+    key = xml2::xml_attr(key_tree, "KEY"),
+    type = xml2::xml_attr(key_tree, "TYPE")
+  )
+  
+  # extract extended attrs
+  extended_tree <- xml2::xml_find_first(playlist_entries, xpath = ".//EXTENDEDDATA")
+  extended <- tibble::tibble(
+    deck = xml2::xml_attr(extended_tree, "DECK"),
+    duration = xml2::xml_attr(extended_tree, "DURATION"),
+    extended_type = xml2::xml_attr(extended_tree, "EXTENDEDTYPE"),
+    public = xml2::xml_attr(extended_tree, "PLAYEDPUBLIC"),
+    start_date = xml2::xml_attr(extended_tree, "STARTDATE"),
+    start_time = xml2::xml_attr(extended_tree, "STARTTIME")
+  )
+  
+  # bind extracted features
+  playlist_data <- dplyr::bind_cols(key, extended)
+  
+  # join track/playlist info
+  data <- inner_join(track_data, playlist_data, by=c("uid"="key"))
+  
+  # fix col classes
+  data <- data %>%
+    mutate_at(c("duration", "deck"), as.numeric) %>%
+    # arrange by start time
+    arrange(start_date, start_time)
   
   return(data)
   
@@ -84,3 +143,5 @@ read_rekordbox_collection <- function(x) {
   return(data)
   
 }
+
+
