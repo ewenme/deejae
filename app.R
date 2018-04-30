380168eba086c8e7b5b91c79637a230304572244
ewen_ <ewenhenderson@gmail.com>
2018-04-30 23:17:52 +0100

    rm collection funcs

--- app.R
+++ app.R
@@ -25,69 +25,14 @@ ui <- navbarPage(
   
   # overall layout/styles ---------------
   "deejae", theme = shinytheme("paper"),
-  selected = "collection", collapsible = TRUE,
+  selected = "about", collapsible = TRUE,
   useShinyalert(),  # Set up shinyalert
   shinyjs::useShinyjs(), # set up shinyjs
   tags$head(
     # Include custom CSS
     includeCSS("styles.css"),
-    tags$style("#collection_plot{height: calc(100vh - 200px) !important;}"),
     tags$style("#selection_plot{height: calc(100vh - 200px) !important;}")
     ),
-  
-  # collection UI -----------------
-  
-  tabPanel("collection", fluidRow(
-    
-    # user selections
-    
-    column(3, wellPanel(
-      # input: collection type
-      conditionalPanel(
-        condition = "output.collection_cond == true",
-        radioButtons(
-          inputId = "collection_type", label = "select collection",
-          choices = c(rekordbox = "rekordbox", traktor = "traktor")
-          )),
-      # input: collection upload
-      conditionalPanel(
-        condition = "output.collection_cond == true",
-        fileInput(
-          inputId = "collection_upload", label = "upload collection",
-          accept = c(".nml", ".xml"), buttonLabel = "browse",
-          placeholder = "  no file selected", multiple = FALSE)
-        ),
-      # input: x-variable
-      conditionalPanel(
-        condition = "output.collection_cond == false",
-        selectInput(inputId = "xvar", label = "wot 2 plot",
-                    c("artists"="artist_name", "albums"="album_title",
-                      "BPM"="bpm", "release years"="release_year"),
-                    selected = "bpm")
-        ),
-      # input: import date
-      conditionalPanel(
-        condition = "output.collection_cond == false",
-        sliderInput(inputId = "import_date_slider", label = "date added",
-                    min = 2000, max = year(Sys.Date()), step = 1, sep = "",
-                    value = c(2000, year(Sys.Date())))
-        ))),
-    
-    # main panel
-    
-    column(9, tabsetPanel(
-      # collection plot view
-      tabPanel("visualise",
-               tags$br(),
-               withSpinner(plotOutput(outputId = "collection_plot"),
-                           type = 8)
-               ),
-      # collection table view
-      tabPanel("table view",
-               withSpinner(DT::dataTableOutput(outputId = "collection_table"), 
-                           type = 8)
-      )))
-    )),
   
   # selection UI -----------------
   
@@ -178,48 +123,7 @@ server <- function(input, output, session) {
   
   # data objects ----------------------------
   
-  # uploaded user collection
-  collection_data <- reactive({
-    
-    # check for collection upload
-    upload <- input$collection_upload
-    if (is.null(upload)) return(NULL)
-    
-    # read collection data
-    if (input$collection_type == "rekordbox") {
-      
-      df <- read_rekordbox_collection(x = input$collection_upload$datapath)
-      
-    } else if (input$collection_type == "traktor") {
-      
-      df <- read_traktor_collection(x = input$collection_upload$datapath)
-      
-    }
-    
-    # filter collection for dodgy observations
-    df <- df %>%
-      filter(release_year <= year(Sys.Date()), release_year >= 1800,
-             bpm <= 300)
-      
-    return(df)
-    
-  })
-  
-  # collection filtered by app inputs
-  collection_data_filtered <- reactive({
-    
-    df <- collection_data()
-    
-    # respond to user inputs
-    df <- df %>%
-      filter(year(import_date) >= input$import_date_slider[1], 
-             year(import_date) <= input$import_date_slider[2])
-    
-    return(df)
-    
-  })
-  
-  # uploaded traktory history data
+  # uploaded history data
   selection_data <- reactive({
     
     # check for history upload
@@ -331,114 +235,6 @@ server <- function(input, output, session) {
     
   })
   
-  # collection page -----------------
-  
-  # collection upload success pop-up 
-  observeEvent(input$collection_upload, {
-    # Show a modal when the button is pressed
-    shinyalert(title = "collection uploaded.", type = "success",
-               closeOnClickOutside = TRUE)
-  })
-  
-  # update slider input based on user collection
-  observe({
-    
-    req(input$collection_upload)
-    
-    dataset <- collection_data()
-    min_import <- min(year(dataset$import_date))
-    max_import <- max(year(dataset$import_date))
-    
-    filtered_dataset <- collection_data_filtered()
-    min_slide <- min(year(filtered_dataset$import_date))
-    max_slide <- max(year(filtered_dataset$import_date))
-    
-    # Control the value, min, max, and step.
-    # Step size is 2 when input value is even; 1 when value is odd.
-    updateSliderInput(session, "import_date_slider", value = c(min_slide, max_slide),
-                      min = min_import, max = max_import)
-  })
-  
-  # condition to use in the collection conditional UI
-  output$collection_cond <- reactive({
-    is.null(input$collection_upload)
-  })
-  outputOptions(output, "collection_cond", suspendWhenHidden = FALSE)
-  
-  # collection plot view
-  output$collection_plot <- renderPlot({
-    
-    req(input$collection_upload)
-    
-    # init data
-    df <- collection_data_filtered()
-    
-    # set plot dependent on x variable
-    if (input$xvar %in% c("bpm", "release_year")) {
-      
-      # density plot
-      p <- ggplot(data = df, aes_string(x=input$xvar)) +
-        geom_density(colour="#E100FF") +
-        ylab("density") +
-        theme_ipsum(base_family = "Work Sans Regular", grid = "Y",
-                    base_size = 16)
-      
-    } else if (input$xvar %in% c("artist_name", "album_title")) {
-      
-      # bar plot
-      p <- df %>%
-        count(.dots=input$xvar) %>%
-        top_n(10, wt=n) %>%
-        filter(!is.na(input$xvar) | input$xvar != "NA") %>%
-        ggplot(aes_string(x=paste0("reorder(", input$xvar, ", -n)"))) +
-        geom_col(aes(y=n), fill="#E100FF") +
-        ylab("# tracks") +
-        coord_flip() +
-        theme_ipsum(base_family = "Work Sans Regular", grid = "X",
-                    base_size = 16)
-        
-    }
-    
-    # add common plot elements
-    p +
-      labs(title = paste(input$xvar, "popularity in your", input$collection_type, 
-                         "collection,", input$import_date_slider[1], "-", 
-                         input$import_date_slider[2]),
-           x=NULL) +
-      theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"),
-            axis.title.x = element_text(size = 16),
-            axis.title.y = element_text(size = 16))
-      
-    
-  })
-  
-  # collection table view
-  output$collection_table <- DT::renderDataTable({
-
-    # input$collection_upload will be NULL initially. After the user selects
-    # and uploads a file, head of that data file will be shown.
-
-    req(input$collection_upload)
-
-    # create datatable
-    df <- collection_data_filtered()
-    df <- subset(df, select = c(track_title, artist_name, album_title,
-                                bpm, release_year, import_date, last_played,
-                                play_count, track_length_formatted
-                                ))
-    
-    DT::datatable(df, rownames = FALSE,
-                  colnames = c("track", "artist", "album", "bpm", "release year",
-                               "date added", "last played", "play count",
-                               "track time"),
-                  options = list(
-                    order = list(list(1, 'asc')),
-                    dom = 'tp',
-                    pageLength = 5
-                  ))
-  })
-  
-  
   # sets page ------------------------------------------------------
   
   # sets upload success pop-up 
@@ -482,7 +278,7 @@ server <- function(input, output, session) {
         p <- ggplot(data = df, aes_string(x=input$selection_xvar)) +
           geom_density(colour="#E100FF") +
           ylab("density") +
-          theme_ipsum(base_family = "Work Sans Regular", grid = "Y",
+          theme_ipsum(base_family = "Work Sans", grid = "Y",
                       base_size = 16)
         
       } else if (input$selection_xvar %in% c("artist_name")) {
@@ -495,7 +291,7 @@ server <- function(input, output, session) {
           geom_col(aes(y=n), fill="#E100FF") +
           ylab("# tracks") +
           coord_flip() +
-          theme_ipsum(base_family = "Work Sans Regular", grid = "X",
+          theme_ipsum(base_family = "Work Sans", grid = "X",
                       base_size = 16)
       }
       
@@ -527,16 +323,15 @@ server <- function(input, output, session) {
                           label=paste3(artist_name, track_title))) +
       geom_dumbbell(size=obj_size, size_x = obj_size, size_xend = obj_size,
                     color="#e3e2e1", colour_x = "#7F00FF", colour_xend = "#E100FF",
-                    alpha=0.8, dot_guide=TRUE, dot_guide_size=0.25) +
+                    alpha=0.6, dot_guide=TRUE, dot_guide_size=0.25) +
       geom_text_repel(nudge_x = max(df$set_time), size=obj_size, segment.size = 0,
-                      direction = "x", family = "Work Sans Regular") +
-      scale_y_continuous(trans = "reverse", breaks = unique(df$track_no)) +
+                      direction = "x", family = "Work Sans Light") +
+      scale_y_continuous(trans = "reverse") +
       scale_x_time() +
       labs(x="set time", y="track #") +
-      theme_ipsum(base_family = "Work Sans Regular", grid = "X",
+      theme_ipsum(base_family = "Work Sans", grid = "X",
                   base_size = 16) +
-      theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"),
-            axis.title.x = element_text(size = 16),
+      theme(axis.title.x = element_text(size = 16),
             axis.title.y = element_text(size = 16))
     }
   })
